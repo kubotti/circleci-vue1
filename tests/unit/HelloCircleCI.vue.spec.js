2ed44d35fb15beada9ff563d1f66b5932dab63b9
kubotti <kubotad@adempiere.jp>
2020-05-19 18:23:06 +0900

    delete unnecessary code

--- tests/unit/HelloCircleCI.vue.spec.js
+++ tests/unit/HelloCircleCI.vue.spec.js
@@ -19,20 +19,11 @@ describe('HelloCircleCI.vue', () => {
 
     let input_text = 'circleci'
     wrapper.find("input").setValue(input_text)
-    //console.log(wrapper.find("input").html())
-    //expect(wrapper.find("#username").text()).toMatch("circleci1")
-    expect(wrapper.vm.username).toMatch(input_text)
 
-    let b1 = wrapper.find("button")
-    expect(b1.text()).toMatch("Click here")
-    //b1.trigger('click')
     wrapper.find("button").trigger('click')
-
     await wrapper.vm.$nextTick();
-    console.log(wrapper.find("p").text())
+
     expect(wrapper.find("p").text()).toBe(expected_greeting)
     //expect(wrapper.vm.greeting).toMatch(expected_greeting)
-
-
   })
 })
