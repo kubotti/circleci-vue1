c9c7d562636c63602ba4ad57fd9a8f6eca0736ad
kubotti <kubotad@adempiere.jp>
2020-05-19 13:04:06 +0900

    add circleci.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloCircleCI msg="Welcome to Your CircleCI Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloCircleCI from './components/HelloCircleCI.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloCircleCI
   }
 }
 </script>
---
+++ src/components/HelloCircleCI.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    This is a test file for CircleCI.
+    <div>
+    <p>{{ message }}</p>
+    <input v-model="message" />
+    <button v-on:click="click1()">Click here</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloCircleCI',
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      message: 'Hello Vue.js!',
+    }
+  },
+  methods: {
+    click1: function () {
+      this.message = this.message.split('').reverse().join('')
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
